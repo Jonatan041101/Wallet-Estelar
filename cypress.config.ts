f2fdb6a00a3a4fd5fe2c5d36ec89d9315594bb55
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-04 17:57:29 -0300

    test: config cypress add folders

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: '81v51y',
+  // projectId: '81v51y',
   component: {
-    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/component/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
     devServer: {
       framework: 'next',
       bundler: 'webpack',
     },
   },
-
   e2e: {
-    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000/',
     supportFile: false,
     setupNodeEvents(on, config) {
