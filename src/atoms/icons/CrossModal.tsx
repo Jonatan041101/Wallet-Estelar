0e7972790e3c7b5d8629c6a21741ac7b9488682d
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-07 15:15:04 -0300

    add icons components

---
+++ src/atoms/icons/CrossModal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function CrossModal() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 6L6 18M6 6l12 12"></path>
+    </svg>
+  );
+}
---
+++ src/atoms/icons/Moon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Moon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+    </svg>
+  );
+}
