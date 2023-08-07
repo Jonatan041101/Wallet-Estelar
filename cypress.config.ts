8992efcd88fd6c67bcf11c63fefd80566cf2efff
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-07 10:55:37 -0300

    fix: delete all comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // projectId: '81v51y',
   component: {
     specPattern: 'cypress/component/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
@@ -14,8 +13,6 @@ export default defineConfig({
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000/',
     supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
