dea43d579a37215cc5ce584084ff2196a71a8ad3
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-07 15:22:19 -0300

    feat: add atom-warning-title-component

---
+++ src/atoms/WarningTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icons from './icons';
+
+interface Props {
+  text: string;
+}
+
+export default function WarningTitle({ text }: Props) {
+  return (
+    <h3 className="warning__h3">
+      <i className="warning__i">
+        <Icons icon="cross" />
+      </i>
+      {text}
+    </h3>
+  );
+}
