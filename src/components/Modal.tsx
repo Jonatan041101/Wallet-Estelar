265c70179bafe07ef1b92fd86d1299d2e1bcbf01
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-07 15:45:16 -0300

    add component-modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,25 @@
+import Icons from '@/atoms/icons';
+import React from 'react';
+
+interface Props {
+  children: React.ReactNode;
+  closeModal: () => void;
+  title?: string;
+}
+
+export default function Modal({ children, title, closeModal }: Props) {
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <button className="modal__btn" onClick={closeModal}>
+          <i className="modal__close">
+            <Icons icon="cross-modal" />
+          </i>
+        </button>
+        <h3 className="modal__title">{title}</h3>
+        {children}
+      </div>
+    </div>
+  );
+}
+// Generar un nuevo par de llaves
