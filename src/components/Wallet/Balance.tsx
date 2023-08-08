9a4cbcf2977f76acfa9123db5cae88a6b702f9f6
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 18:01:42 -0300

    style: add className jsx the component Balance

--- src/components/Wallet/Balance.tsx
+++ src/components/Wallet/Balance.tsx
@@ -5,19 +5,21 @@ import React from 'react';
 export default function Balance() {
   const account = useData();
   return (
-    <div>
-      <h2>Tu balance</h2>
+    <div className="balance">
+      <h2 className="balance__title">Tu balance</h2>
       {account ? (
-        account.balances.map((balance, index) => (
-          <article key={index}>
-            <p>
-              {balance.balance}{' '}
-              {balance.asset_type === 'native'
-                ? 'Lumens (XLM)'
-                : balance.asset_type}
-            </p>
-          </article>
-        ))
+        <section className="balance__section">
+          {account.balances.map((balance, index) => (
+            <article className="balance__article" key={index}>
+              <p className="balance__p">
+                {balance.balance}{' '}
+                {balance.asset_type === 'native'
+                  ? 'Lumens (XLM)'
+                  : balance.asset_type}
+              </p>
+            </article>
+          ))}
+        </section>
       ) : (
         <p>No tienes fondos</p>
       )}
