f0742edf69e4cd47b6a6dda6ec9372fd92a9c8cc
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 18:40:31 -0300

    feat: add type to response activeaccount

--- src/services/activeAccount.ts
+++ src/services/activeAccount.ts
@@ -1,12 +1,12 @@
+import { FriendbotResponse } from '@/types/response';
+
 const activeAccount = async (publicKey: string) => {
   try {
     const response = await fetch(
       `https://friendbot.stellar.org?addr=${publicKey}`,
     );
     const responseJSON = await response.json();
-    console.log(responseJSON);
-
-    return responseJSON;
+    return responseJSON as FriendbotResponse;
   } catch (error) {
     console.log(error);
   }
