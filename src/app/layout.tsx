b61a4f160d6f4c677d16c6d0afd83547c1606ee6
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 18:54:32 -0300

    fix: move ToastContainer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../css/main.css';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} body`}>{children}</body>
+      <body className={`${inter.className} body`}>
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
