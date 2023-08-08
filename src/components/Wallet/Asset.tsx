7ee6375bcbaa84f0300c8adb43a6c05d2331c499
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 19:21:31 -0300

    feat: create component Asset

---
+++ src/components/Wallet/Asset.tsx
@@ -0,0 +1,26 @@
+import Button from '@/atoms/Button';
+import React from 'react';
+import { Horizon } from 'stellar-sdk';
+interface Props {
+  balance:
+    | Horizon.BalanceLineNative
+    | Horizon.BalanceLineAsset<'credit_alphanum4'>
+    | Horizon.BalanceLineAsset<'credit_alphanum12'>
+    | Horizon.BalanceLineLiquidityPool;
+}
+export default function Asset({ balance }: Props) {
+  return (
+    <article className="balance__article">
+      <p className="balance__p">
+        {balance.balance}{' '}
+        {balance.asset_type === 'native' ? 'Lumens (XLM)' : balance.asset_type}
+      </p>
+      <Button
+        classNameBtn="button__complete"
+        text="Enviar"
+        handleClick={() => {}}
+        icon="send"
+      />
+    </article>
+  );
+}
