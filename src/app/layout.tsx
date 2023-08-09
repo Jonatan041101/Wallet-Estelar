57f8816ba673d46aa78b85e6ae2e5a966f5d6888
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 14:27:20 -0300

    feat: add Header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import '../css/main.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Header from '@/components/Header/Header';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} body`}>
+        <Header />
         {children}
         <ToastContainer />
       </body>
