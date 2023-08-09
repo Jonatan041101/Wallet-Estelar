9aa5a4f867c16aed42739ce36381dcb99052595f
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 19:44:37 -0300

    feadr:add  Asset component form to send the transaction

--- src/components/Wallet/Asset.tsx
+++ src/components/Wallet/Asset.tsx
@@ -1,6 +1,11 @@
+'use client';
 import Button from '@/atoms/Button';
-import React from 'react';
+import React, { useState } from 'react';
 import { Horizon } from 'stellar-sdk';
+import Modal from '../Modal';
+import useBoolean from '@/hooks/useBoolean';
+import Form from '../Form';
+import Input from '@/atoms/Input';
 interface Props {
   balance:
     | Horizon.BalanceLineNative
@@ -8,19 +13,69 @@ interface Props {
     | Horizon.BalanceLineAsset<'credit_alphanum12'>
     | Horizon.BalanceLineLiquidityPool;
 }
+
+interface Transaction {
+  publicKey: string;
+  amount: string;
+}
+interface State {
+  transaction: Transaction;
+}
+const INITIAL_STATE: State['transaction'] = {
+  amount: '',
+  publicKey: '',
+};
 export default function Asset({ balance }: Props) {
+  const [{ amount, publicKey }, setTransaction] =
+    useState<State['transaction']>(INITIAL_STATE);
+  const { view, handleChangeBoolean } = useBoolean();
+  const asset =
+    balance.asset_type === 'native' ? 'Lumens (XLM)' : balance.asset_type;
+  const handleSendTransaction = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+  const handleChangeTransaction = (
+    evt: React.ChangeEvent<HTMLInputElement>,
+  ) => {};
   return (
     <article className="balance__article">
       <p className="balance__p">
-        {balance.balance}{' '}
-        {balance.asset_type === 'native' ? 'Lumens (XLM)' : balance.asset_type}
+        {balance.balance} {asset}
       </p>
       <Button
         classNameBtn="button__complete"
         text="Enviar"
-        handleClick={() => {}}
+        handleClick={handleChangeBoolean}
         icon="Send"
       />
+      {view && (
+        <Modal closeModal={handleChangeBoolean}>
+          <Form handleSubmit={handleSendTransaction}>
+            <Input
+              name="publicKey"
+              handleChange={handleChangeTransaction}
+              labelText="Pubic Key"
+              placeholder=""
+              type="text"
+              value={publicKey}
+            />
+            <Input
+              name="amount"
+              handleChange={handleChangeTransaction}
+              labelText={`Cantidad de ${asset} para enviar`}
+              placeholder=""
+              type="number"
+              value={amount}
+            />
+            <Button
+              handleClick={() => {}}
+              classNameBtn="button__complete"
+              text="Enviar"
+              icon="Send"
+            />
+          </Form>
+        </Modal>
+      )}
     </article>
   );
 }
