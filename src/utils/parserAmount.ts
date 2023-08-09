c99f9ac17fc579d64ce366da99182c009dbd19ac
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 20:05:45 -0300

    feat: fix parserAmount

--- src/utils/parserAmount.ts
+++ src/utils/parserAmount.ts
@@ -2,5 +2,5 @@ const parserAmountToDecimal = (amount: string) => {
   const indexDecimal = amount.indexOf('.');
   const getInt = amount.slice(0, indexDecimal);
   const getDecimal = amount.slice(indexDecimal).padEnd(8, '0');
-  return getInt.concat(getDecimal);
+  return indexDecimal === -1 ? `${amount}.0000000` : getInt.concat(getDecimal);
 };
