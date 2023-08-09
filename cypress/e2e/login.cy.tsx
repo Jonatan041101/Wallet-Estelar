a9f5ae73d4875ef9ed170d03cbae76d15aaeefaa
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 22:32:51 -0300

    test: add it login verify and activeaccount

--- cypress/e2e/login.cy.tsx
+++ cypress/e2e/login.cy.tsx
@@ -1,10 +1,12 @@
 const buttonText = 'Conectar con una clave secreta';
 const textLiLoginConfirm =
   'Copiar y pegar su clave secreta lo hace vulnerable a accidentes, ataques y estafas que pueden provocar la pérdida de fondos.';
+const secretKeyFail = 'AASDASD0ASDASD0ASDASD0ASDSA0DASDJASDAKS';
 describe('Button Component', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.contains('button', buttonText).click();
+    cy.contains('button', buttonText).should('be.visible');
+    cy.contains('button', buttonText).click({ force: true });
   });
 
   it('Should render the button text correctly', () => {
@@ -47,5 +49,61 @@ describe('Button Component', () => {
     cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
     cy.get('.modal__btn').click();
     cy.get('.modal__btn').should('not.exist');
+  });
+  it('Should log in by entering the secret key and changing to the wallet path', () => {
+    cy.get('.check__input').should('exist');
+    cy.get('.check__input').should('not.be.checked');
+    cy.contains('button', 'Continuar').should('exist');
+    cy.get('.check__input').check();
+    cy.get('.check__input').should('be.checked');
+    cy.contains('button', 'Continuar').click();
+    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
+    cy.get('input[type="password"]').type(Cypress.env('secret_key'));
+    cy.get('button[class="button button__complete"]').click();
+    cy.wait(3000);
+    cy.contains('h3', 'Su clave pública estelar').should('be.visible');
+  });
+  it('Should log in by entering the secret key and changing to the wallet path', () => {
+    cy.get('.check__input').should('exist');
+    cy.get('.check__input').should('not.be.checked');
+    cy.contains('button', 'Continuar').should('exist');
+    cy.get('.check__input').check();
+    cy.get('.check__input').should('be.checked');
+    cy.contains('button', 'Continuar').click();
+    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
+    cy.get('input[type="password"]').type(Cypress.env('secret_key'));
+    cy.get('button[class="button button__complete"]').click();
+    cy.wait(3000);
+    cy.contains('h3', 'Su clave pública estelar').should('be.visible');
+  });
+  it.only('Should give an error message when entering an invalid secret key', () => {
+    cy.get('.check__input').should('exist');
+    cy.get('.check__input').should('not.be.checked');
+    cy.contains('button', 'Continuar').should('exist');
+    cy.get('.check__input').check();
+    cy.get('.check__input').should('be.checked');
+    cy.contains('button', 'Continuar').click();
+    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
+    cy.get('input[type="password"]').type(secretKeyFail);
+    cy.get('button[class="button button__complete"]').click();
+    cy.wait(1000);
+    cy.contains('La llave secreta que a ingresado es incorrecta');
+  });
+  it('Should show a message that the account has been activated when clicking on the activate account button', () => {
+    cy.get('.check__input').should('exist');
+    cy.get('.check__input').should('not.be.checked');
+    cy.contains('button', 'Continuar').should('exist');
+    cy.get('.check__input').check();
+    cy.get('.check__input').should('be.checked');
+    cy.contains('button', 'Continuar').click();
+    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
+    cy.get('input[type="password"]').type(Cypress.env('secret_key'));
+    cy.get('button[class="button button__complete"]').click();
+    cy.wait(3000);
+    cy.contains('h3', 'Su clave pública estelar').should('be.visible');
+    cy.contains('button', 'Activar Cuenta').should('exist');
+    cy.contains('button', 'Activar Cuenta').click();
+    cy.wait(8000);
+    cy.contains('La cuenta a sido activada').should('be.visible');
   });
 });
