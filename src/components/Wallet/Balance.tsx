45c8a0a46014ce27081fcbc271c857edc66207c5
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 23:19:01 -0300

    fix: change name variable account for balanceAccount

--- src/components/Wallet/Balance.tsx
+++ src/components/Wallet/Balance.tsx
@@ -1,17 +1,16 @@
 'use client';
-import Button from '@/atoms/Button';
 import useData from '@/hooks/useData';
 import React from 'react';
 import Asset from './Asset';
 
 export default function Balance() {
-  const account = useData();
+  const { balanceAccount } = useData();
   return (
     <div className="balance">
       <h2 className="balance__title">Tu balance</h2>
-      {account ? (
+      {balanceAccount ? (
         <section className="balance__section">
-          {account.balances.map((balance, index) => (
+          {balanceAccount.balances.map((balance, index) => (
             <Asset balance={balance} key={index} />
           ))}
         </section>
