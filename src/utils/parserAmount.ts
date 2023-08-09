e31e0356ee02ae74c0e0ea60d5f42f92fa163baa
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 20:07:15 -0300

    fix: export function

--- src/utils/parserAmount.ts
+++ src/utils/parserAmount.ts
@@ -1,4 +1,4 @@
-const parserAmountToDecimal = (amount: string) => {
+export const parserAmountToDecimal = (amount: string) => {
   const indexDecimal = amount.indexOf('.');
   const getInt = amount.slice(0, indexDecimal);
   const getDecimal = amount.slice(indexDecimal).padEnd(8, '0');
