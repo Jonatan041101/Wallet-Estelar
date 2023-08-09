a6648142093ef3506298c21ad638177a2f1cc6b6
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 00:44:01 -0300

    refactor: toastOptions create variable for notification options

--- src/utils/toastMsg.ts
+++ src/utils/toastMsg.ts
@@ -1,33 +1,27 @@
 import { toast } from 'react-toastify';
+import type { ToastOptions } from 'react-toastify';
 import {
   MessageError,
   MessageSucces,
   MessageSuccessWithVariable,
 } from './constants';
 
+export const toastOptions: ToastOptions = {
+  position: 'bottom-center',
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'light',
+};
+
 export const errorMsg = (message: MessageError) => {
-  toast.error(`🚫 ${message}`, {
-    position: 'bottom-center',
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  });
+  toast.error(`🚫 ${message}`, toastOptions);
 };
 export const successMsg = (
   message: MessageSucces | MessageSuccessWithVariable,
 ) => {
-  toast.success(`✅ ${message}`, {
-    position: 'bottom-center',
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  });
+  toast.success(`✅ ${message}`, toastOptions);
 };
