4cfe3ce59f2d7424a520387349332767af26c713
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-08 23:09:38 -0300

    feat: accountSlice add balanceAccount

--- src/store/accountSlice.ts
+++ src/store/accountSlice.ts
@@ -1,9 +1,12 @@
 import { AccountGenerate } from '@/types/types';
+import { AccountResponse } from 'stellar-sdk';
 import { StateCreator } from 'zustand';
 
 interface Wallet {
   account: AccountGenerate;
+  balanceAccount: AccountResponse | null;
   getAcc: (account: AccountGenerate) => void;
+  changeBalanceAccount: (balanceAccount: AccountResponse) => void;
 }
 
 export const accountSlice: StateCreator<Wallet> = (set) => ({
@@ -11,6 +14,7 @@ export const accountSlice: StateCreator<Wallet> = (set) => ({
     publicKey: '',
     secretKey: '',
   },
+  balanceAccount: null,
   getAcc: (account) => {
     set((state) => ({
       ...state,
@@ -19,5 +23,8 @@ export const accountSlice: StateCreator<Wallet> = (set) => ({
         secretKey: account.secretKey,
       },
     }));
+  },
+  changeBalanceAccount: (balanceAccount) => {
+    set((state) => ({ ...state, balanceAccount }));
   },
 });
