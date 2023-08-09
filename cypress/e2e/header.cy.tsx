1ad3b62a0bd7197e5078999ab1cb92c5cd9f1f53
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 17:48:28 -0300

    test: fix test header

--- cypress/e2e/header.cy.tsx
+++ cypress/e2e/header.cy.tsx
@@ -1,20 +1,19 @@
 describe('Header Component', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.contains('button', 'Conectar con una clave secreta').should(
-      'be.visible',
-    );
     cy.contains('button', 'Conectar con una clave secreta').click();
+    cy.get('.check__input').should('exist').as('checkboxInput');
   });
+
   it('Should see the header', () => {
     cy.contains('h3', 'Visor de cuenta').should('be.visible');
   });
+
   it('Should profile seen when you login', () => {
-    cy.get('.check__input').should('exist');
-    cy.get('.check__input').should('not.be.checked');
+    cy.get('@checkboxInput').should('not.be.checked');
     cy.contains('button', 'Continuar').should('exist');
-    cy.get('.check__input').check();
-    cy.get('.check__input').should('be.checked');
+    cy.get('@checkboxInput').check();
+    cy.get('@checkboxInput').should('be.checked');
     cy.contains('button', 'Continuar').click();
     cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
     cy.get('input[type="password"]').type(Cypress.env('secret_key'));
@@ -22,37 +21,43 @@ describe('Header Component', () => {
     cy.wait(3000);
     cy.get('div[class="profile__img"]').should('be.visible');
   });
+
   it('Should log out when the log out button is clicked', () => {
-    cy.get('.check__input').should('exist');
-    cy.get('.check__input').should('not.be.checked');
+    cy.get('@checkboxInput').should('exist');
+    cy.get('@checkboxInput').should('not.be.checked');
     cy.contains('button', 'Continuar').should('exist');
-    cy.get('.check__input').check();
-    cy.get('.check__input').should('be.checked');
+    cy.get('@checkboxInput').check();
+    cy.get('@checkboxInput').should('be.checked');
     cy.contains('button', 'Continuar').click();
     cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
     cy.get('input[type="password"]').type(Cypress.env('secret_key'));
     cy.get('button[class="button button__complete"]').click();
     cy.wait(3000);
     cy.get('div[class="profile__img"]').should('be.visible');
-    cy.contains('button', 'Cerrar sesión').should('be.visible');
-    cy.contains('button', 'Cerrar sesión').should('be.visible').click();
+    cy.contains('button', 'Cerrar sesión')
+      .should('be.visible')
+      .as('logoutButton');
+    cy.get('@logoutButton').click();
     cy.wait(3000);
-    cy.contains('button', 'Cerrar sesión').should('not.exist');
+    cy.get('@logoutButton').should('not.exist');
   });
+
   it('Should copy the public key when you click on it', () => {
-    cy.get('.check__input').should('exist');
-    cy.get('.check__input').should('not.be.checked');
+    cy.get('@checkboxInput').should('exist');
+    cy.get('@checkboxInput').should('not.be.checked');
     cy.contains('button', 'Continuar').should('exist');
-    cy.get('.check__input').check();
-    cy.get('.check__input').should('be.checked');
+    cy.get('@checkboxInput').check();
+    cy.get('@checkboxInput').should('be.checked');
     cy.contains('button', 'Continuar').click();
     cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
     cy.get('input[type="password"]').type(Cypress.env('secret_key'));
     cy.get('button[class="button button__complete"]').click();
     cy.wait(3000);
     cy.get('div[class="profile__img"]').should('be.visible');
-    cy.get('button[data-testid="public-key"]').should('be.visible');
-    cy.get('button[data-testid="public-key"]').should('be.visible').click();
+    cy.get('button[data-testid="public-key"]')
+      .should('be.visible')
+      .as('publicKeyButton');
+    cy.get('@publicKeyButton').click();
     cy.wait(500);
     cy.contains('Copiado al portapapeles').should('be.visible');
   });
