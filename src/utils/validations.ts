1f1e74573d64f4d56a85c32acd38496fb9c8f76e
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-09 10:59:02 -0300

    feat: add regexp publicKey

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,6 +1,8 @@
 interface Validation {
   secretKey: RegExp;
+  publicKey: RegExp;
 }
 export const VALIDATIONS: Validation = {
   secretKey: /^S[A-Za-z0-9]{55}$/i,
+  publicKey: /^G[A-Za-z0-9]{55}$/i,
 };
