87dc6e6eb3c99f0e89762935e29b4fb01f92c95e
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-10 16:36:40 -0300

    fix: add getTransactions

--- src/store/paymentSlice.ts
+++ src/store/paymentSlice.ts
@@ -5,12 +5,16 @@ type Payment = 'Albedo' | 'Only-Stellar';
 export interface TypePayment {
   payment: Payment;
   transactions: ServerApi.PaymentOperationRecord[];
+  getTransactions: (transactions: ServerApi.PaymentOperationRecord[]) => void;
   changePayment: (payment: Payment) => void;
 }
 
 export const paymentSlice: StateCreator<TypePayment> = (set) => ({
   payment: 'Only-Stellar',
   transactions: [],
+  getTransactions: (transactions) => {
+    set((state) => ({ ...state, transactions }));
+  },
   changePayment: (payment) => {
     set((state) => ({ ...state, payment }));
   },
