9676ba627224de188aa8fc6f56e1a682e0d5bde5
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-10 02:19:20 -0300

    feat: add button reload balance account

--- src/components/Wallet/Balance.tsx
+++ src/components/Wallet/Balance.tsx
@@ -3,14 +3,22 @@ import useLoadAccount from '@/hooks/useLoadAccount';
 import React from 'react';
 import Asset from './Asset';
 import useAccount from '@/hooks/useAccount';
+import Button from '@/atoms/Button';
 
 export default function Balance() {
-  const { balanceAccount } = useLoadAccount();
+  const { balanceAccount, getData } = useLoadAccount();
   useAccount();
 
   return (
     <div className="balance">
-      <h2 className="balance__title">Tu balance</h2>
+      <div className="balance__top">
+        <h2 className="balance__title">Tu balance</h2>
+        <Button
+          text="Recargar"
+          classNameBtn="button__complete"
+          handleClick={getData}
+        />
+      </div>
       {balanceAccount ? (
         <section className="balance__section">
           {balanceAccount.balances.map((balance, index) => (
