52225f6ac1de18b6024133c826782d85b0c5bd13
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-10 11:10:05 -0300

    text: fix add alias and login user in beforeEach of payment

--- cypress/e2e/payment.cy.tsx
+++ cypress/e2e/payment.cy.tsx
@@ -3,9 +3,6 @@ describe('Payment', () => {
     cy.visit('/');
     cy.contains('button', 'Conectar con una clave secreta').click();
     cy.get('.check__input').should('exist').as('checkboxInput');
-  });
-
-  it('Should send the transaction', () => {
     cy.get('@checkboxInput').should('not.be.checked');
     cy.contains('button', 'Continuar').should('exist');
     cy.get('@checkboxInput').check();
@@ -16,15 +13,18 @@ describe('Payment', () => {
     cy.get('button[class="button button__complete"]').click();
     cy.wait(3000);
     cy.get('div[class="profile__img"]').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible').click();
+    cy.contains('button', 'Enviar').should('be.visible').as('buttonAsset');
+    cy.get('@buttonAsset').click();
     cy.get('input[name="publicKey"]').should('be.visible').as('inputPublicKey');
-    cy.get('input[name="amount"]').should('be.visible').as('inputAssets');
-    cy.get('@inputPublicKey').type(Cypress.env('public_key_send'));
-    cy.get('@inputAssets').should('be.visible').type('200');
+    cy.get('input[name="amount"]').as('inputAssets');
     cy.get('button[data-testid="send-transaction"]')
       .should('be.visible')
       .as('buttonSend');
+  });
+
+  it('Should send the transaction', () => {
+    cy.get('@inputPublicKey').type(Cypress.env('public_key_send'));
+    cy.get('@inputAssets').type('200');
     cy.get('@buttonSend').click();
     cy.contains('Espere un momento a que se termine la transacción').should(
       'be.visible',
@@ -34,45 +34,13 @@ describe('Payment', () => {
   });
 
   it("Should give an error message when you don't type an amount", () => {
-    cy.get('@checkboxInput').should('not.be.checked');
-    cy.contains('button', 'Continuar').should('exist');
-    cy.get('@checkboxInput').check();
-    cy.get('@checkboxInput').should('be.checked');
-    cy.contains('button', 'Continuar').click();
-    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
-    cy.get('input[type="password"]').type(Cypress.env('secret_key'));
-    cy.get('button[class="button button__complete"]').click();
-    cy.wait(3000);
-    cy.get('div[class="profile__img"]').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible').click();
-    cy.get('input[name="publicKey"]').should('be.visible').as('inputPublicKey');
     cy.get('@inputPublicKey').type(Cypress.env('public_key_send'));
-    cy.get('button[data-testid="send-transaction"]')
-      .should('be.visible')
-      .as('buttonSend');
     cy.get('@buttonSend').click();
     cy.contains('El valor ingresado no es numero').should('be.visible');
   });
 
   it("Should give an error message when you don't type a public key", () => {
-    cy.get('@checkboxInput').should('not.be.checked');
-    cy.contains('button', 'Continuar').should('exist');
-    cy.get('@checkboxInput').check();
-    cy.get('@checkboxInput').should('be.checked');
-    cy.contains('button', 'Continuar').click();
-    cy.contains('label', 'TU CLAVE SECRETA').should('be.visible');
-    cy.get('input[type="password"]').type(Cypress.env('secret_key'));
-    cy.get('button[class="button button__complete"]').click();
-    cy.wait(3000);
-    cy.get('div[class="profile__img"]').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible');
-    cy.contains('button', 'Enviar').should('be.visible').click();
-    cy.get('input[name="amount"]').should('be.visible').as('inputAssets');
-    cy.get('@inputAssets').should('be.visible').type('200');
-    cy.get('button[data-testid="send-transaction"]')
-      .should('be.visible')
-      .as('buttonSend');
+    cy.get('@inputAssets').type('200');
     cy.get('@buttonSend').click();
     cy.contains('La llave publica que ha ingresado es incorrecta').should(
       'be.visible',
