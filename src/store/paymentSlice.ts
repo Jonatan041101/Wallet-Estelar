c36a2281aecef690979aa5df24dd4602725b4a71
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-10 16:00:30 -0300

    feat: add transactions store

--- src/store/paymentSlice.ts
+++ src/store/paymentSlice.ts
@@ -1,13 +1,16 @@
+import type { ServerApi } from 'stellar-sdk';
 import { StateCreator } from 'zustand';
 
 type Payment = 'Albedo' | 'Only-Stellar';
 export interface TypePayment {
   payment: Payment;
+  transactions: ServerApi.PaymentOperationRecord[];
   changePayment: (payment: Payment) => void;
 }
 
 export const paymentSlice: StateCreator<TypePayment> = (set) => ({
   payment: 'Only-Stellar',
+  transactions: [],
   changePayment: (payment) => {
     set((state) => ({ ...state, payment }));
   },
