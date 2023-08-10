6d2542b8284e614b034f279e5827eb31d3a8e86b
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-10 15:59:29 -0300

    refactor: rename fucntions loadPayments

---
+++ src/services/loadTransactions.ts
@@ -0,0 +1,10 @@
+import { server } from '@/utils/server';
+
+export const loadTransactions = async (publicKey: string) => {
+  try {
+    const transactions = await server.payments().forAccount(publicKey).call();
+    return transactions.records;
+  } catch (error) {
+    console.log(error);
+  }
+};
