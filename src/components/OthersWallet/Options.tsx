41e5cebac08266f0a4e6c32875374bbe61f0d7e4
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-11 12:51:13 -0300

    refactor: component Options

--- src/components/OthersWallet/Options.tsx
+++ src/components/OthersWallet/Options.tsx
@@ -2,12 +2,7 @@
 import React from 'react';
 import OthersWallet from './OthersWallet';
 import { IconTypes } from '@/types/icons';
-import { useBearStore } from '@/store/store';
-import useNavigate from '@/hooks/useNavigate';
-import type { AccountGenerate } from '@/types/types';
-import { verifyMessageSignature } from '@albedo-link/signature-verification';
-import { errorMsg } from '@/utils/toastMsg';
-import { MessageError } from '@/utils/constants';
+import useLogin from '@/hooks/useLogin';
 
 export interface Wallet {
   icon: IconTypes;
@@ -15,41 +10,10 @@ export interface Wallet {
   login: () => Promise<void>;
 }
 export default function Options() {
-  const { getAccount, changePayment } = useBearStore(
-    ({ getAcc, changePayment }) => ({
-      getAccount: getAcc,
-      changePayment,
-    }),
-  );
-  const { handleNavigate } = useNavigate();
-  const loginAlbedo = async () => {
-    try {
-      const albedo = await import('@albedo-link/intent');
-      const res = await albedo.default.publicKey({});
-      await albedo.default.signMessage({
-        message: res.signed_message,
-      });
-      const isValid = verifyMessageSignature(
-        res.pubkey,
-        res.signed_message,
-        res.signature,
-      );
-      if (!isValid) return errorMsg(MessageError.ERROR_PUBLIC_KEY);
-      const pairKeys: AccountGenerate = {
-        publicKey: res.pubkey,
-        secretKey: '',
-      };
+  const { albedo } = useLogin();
 
-      getAccount(pairKeys);
-
-      handleNavigate('/wallet');
-      changePayment('Albedo');
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const wallets: Wallet[] = [
-    { icon: 'Albedo', text: 'Conéctate con Albedo', login: loginAlbedo },
+    { icon: 'Albedo', text: 'Conéctate con Albedo', login: albedo },
   ];
   return (
     <section className="otherwallet">
