c15951ac00e3919099855a6657e9ac3a339b80d6
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-11 13:11:52 -0300

    refactor: usePayment

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -5,9 +5,33 @@ import {
 } from '@/services/payment';
 import { useBearStore } from '@/store/store';
 import { MessageError } from '@/utils/constants';
+import { Horizon } from 'stellar-sdk';
 
 export default function usePayment() {
   const { payment } = useBearStore(({ payment }) => ({ payment }));
+  const transaction = async (
+    key: string,
+    destination: string,
+    amount: string,
+    sendTransaction: (
+      key: string,
+      destination: string,
+      amount: string,
+    ) => Promise<Horizon.SubmitTransactionResponse>,
+  ): Promise<boolean> => {
+    try {
+      let signature: boolean = false;
+      const signatureTransaction = await sendTransaction(
+        key,
+        destination,
+        amount,
+      );
+      signature = signatureTransaction.successful;
+      return signature;
+    } catch (error) {
+      throw new TransactionError(MessageError.ERROR_IN_TRANSACTION);
+    }
+  };
   const handleTransaction = async (
     key: string,
     destination: string,
@@ -16,20 +40,20 @@ export default function usePayment() {
     try {
       let signature: boolean = false;
       if (payment === 'Albedo') {
-        const signatureTransaction = await sendTransactionWithAlbedo(
+        signature = await transaction(
           key,
           destination,
           amount,
+          sendTransactionWithAlbedo,
         );
-        signature = signatureTransaction.successful;
       }
       if (payment === 'Only-Stellar') {
-        const signatureTransaction = await sendTransactionOnlyStellar(
+        signature = await transaction(
           key,
           destination,
           amount,
+          sendTransactionOnlyStellar,
         );
-        signature = signatureTransaction.successful;
       }
       if (!signature)
         throw new TransactionError(MessageError.ERROR_IN_TRANSACTION);
