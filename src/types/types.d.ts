c8b73aef3e5bd94c5e0429c6b0a151f45b2a5e96
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-11 12:25:53 -0300

    feat: add types Horizon

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,11 @@
+import type { Horizon } from 'stellar-sdk';
+
 export interface AccountGenerate {
   secretKey: string;
   publicKey: string;
 }
+export type BalanceProp =
+  | Horizon.BalanceLineNative
+  | Horizon.BalanceLineAsset<'credit_alphanum4'>
+  | Horizon.BalanceLineAsset<'credit_alphanum12'>
+  | Horizon.BalanceLineLiquidityPool;
