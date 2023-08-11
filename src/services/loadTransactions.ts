e5e9e9845d46aed5b15b13cbd519a68ee5c99028
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-11 13:19:26 -0300

    refactor: loadTransactions

--- src/services/loadTransactions.ts
+++ src/services/loadTransactions.ts
@@ -1,10 +1,14 @@
+import { LoadError } from '@/helpers/handlerError';
+import { MessageError } from '@/utils/constants';
 import { server } from '@/utils/server';
 
 export const loadTransactions = async (publicKey: string) => {
   try {
-    const transactions = await server.payments().forAccount(publicKey).call();
-    return transactions.records;
+    const payments = server.payments();
+    const transactions = payments.forAccount(publicKey);
+    const allTransactions = await transactions.call();
+    return allTransactions.records;
   } catch (error) {
-    console.error(error);
+    throw new LoadError(MessageError.LOAD_ERROR);
   }
 };
