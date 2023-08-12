34b132c439818a48e184c0c28c5ee8214ffc90d9
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-12 18:40:47 -0300

    refactor: hook usePayment

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,29 +1,32 @@
 import { TransactionError } from '@/helpers/handlerError';
 import { useBearStore } from '@/store/store';
 import { MessageError } from '@/utils/constants';
-import { paymentsMethod } from '@/utils/paymentMethods';
+import { paymentsMethod } from '@/utils/transactionsMethod';
+import { AccountResponse, Keypair } from 'stellar-sdk';
 
 export default function usePayment() {
   const { payment } = useBearStore(({ payment }) => ({ payment }));
 
   const handleTransaction = async (
-    key: string,
+    account: AccountResponse,
     destination: string,
     amount: string,
   ) => {
     try {
-      const { successful } = await paymentsMethod[payment](
-        key,
+      const transaction = await paymentsMethod[payment](
+        account,
         destination,
         amount,
       );
-      if (!successful)
-        throw new TransactionError(MessageError.ERROR_IN_TRANSACTION);
+      // if (!successful)
+      //   throw new TransactionError(MessageError.ERROR_IN_TRANSACTION);
+      return transaction;
     } catch (error) {
       console.error(error);
       if (error instanceof TransactionError) {
         throw new TransactionError(error.message as MessageError);
       }
+      throw new Error('');
     }
   };
   return {
