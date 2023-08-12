20262285e95fe51187d37f4296d205b21d9706f4
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-12 18:41:45 -0300

    fix: remove comments

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -2,7 +2,7 @@ import { TransactionError } from '@/helpers/handlerError';
 import { useBearStore } from '@/store/store';
 import { MessageError } from '@/utils/constants';
 import { paymentsMethod } from '@/utils/transactionsMethod';
-import { AccountResponse, Keypair } from 'stellar-sdk';
+import { AccountResponse } from 'stellar-sdk';
 
 export default function usePayment() {
   const { payment } = useBearStore(({ payment }) => ({ payment }));
@@ -18,15 +18,13 @@ export default function usePayment() {
         destination,
         amount,
       );
-      // if (!successful)
-      //   throw new TransactionError(MessageError.ERROR_IN_TRANSACTION);
       return transaction;
     } catch (error) {
       console.error(error);
       if (error instanceof TransactionError) {
         throw new TransactionError(error.message as MessageError);
       }
-      throw new Error('');
+      throw new Error(MessageError.ERROR);
     }
   };
   return {
